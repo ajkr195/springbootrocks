a21c266d4d1511a030c82af66fac247146349a37
ajkr195 <34718091+ajkr195@users.noreply.github.com>
2025-03-06 13:10:13 -0500

    Delete src directory

--- src/main/java/com/spring/boot/rocks/jdbc/AppUserJDBCService.java
+++
@@ -1,9 +0,0 @@
-package com.spring.boot.rocks.jdbc;
-
-import java.util.List;
-
-import com.spring.boot.rocks.model.AppUser;
-
-public interface AppUserJDBCService {
-	public List<AppUser> getAllAppUsers();
-}
--- src/main/java/com/spring/boot/rocks/service/AppRoleService.java
+++
@@ -1,9 +0,0 @@
-package com.spring.boot.rocks.service;
-
-import com.spring.boot.rocks.model.AppRole;
-
-public interface AppRoleService {
-
-	AppRole findByid(int id);
-
-}
