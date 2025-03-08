bcd9074492d22572e35eaa0417c43270914e9de0
Ajay Kumar <Ajay Kumar@DESKTOP-2EDS8Q5>
2025-03-08 02:57:40 -0500

    Spring Boot 3.4.x

--- src/main/java/com/spring/boot/rocks/Application.java
+++ src/main/java/com/spring/boot/rocks/Application.java
@@ -8,6 +8,8 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		System.out.println(
+				"\nApplication URL is: http://localhost:8080\nUsername is: admin@admin\nPassword is: admin@admin");
 	}
 
 }
