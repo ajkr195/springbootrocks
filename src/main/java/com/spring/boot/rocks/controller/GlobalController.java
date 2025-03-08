0c5184366a700ca51b175ace7fc4468e68df0606
Ajay Kumar <Ajay Kumar@DESKTOP-2EDS8Q5>
2025-03-08 14:49:05 -0500

    Spring Boot 3.4.x

--- src/main/java/com/spring/boot/rocks/controller/GlobalController.java
+++ src/main/java/com/spring/boot/rocks/controller/GlobalController.java
@@ -4,12 +4,12 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 @ControllerAdvice
-public class Global {
-
+public class GlobalController {
 
 	@ModelAttribute("applicationName")
 	public String getapplicationName() {
 		return "SBOOT";
 	}
 
+	
 }
